e6057a75edc8585580ed5294b251fae39ff19e87
Tina Flower <tmflower@gmail.com>
2022-10-04 16:52:08 -1000

    removed extra list tags from list component

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -3,20 +3,10 @@ import { ListItem } from '../components';
 export function List({ data }) {
 	return (
 		<>
-			{/* <p>
-				Hello from the <code>/list</code> page!
-			</p> */}
 			<ul>
 				{data.map((item, i) => (
-					<li>
-						<ListItem key={item.name + i} {...item} />
-					</li>
+					<ListItem key={item.name + i} {...item} />
 				))}
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
 			</ul>
 		</>
 	);
