a4a9118dfd28872204b467628544f1774c128b34
Weston Norwood <weston.norwood@gmail.com>
2022-10-14 18:26:40 -0700

    truthy test of listToken for conditional redirect

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,15 @@
 import './Home.css';
 import { Navigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 export function Home({ createNewList, listToken }) {
-	const handleClick = () => {
+	const handleClick = useCallback(() => {
 		createNewList();
-	};
+	}, [createNewList]);
 
 	return (
 		<div className="Home">
-			{listToken != null && <Navigate to={'/List'} replace={true} />}
+			{listToken && <Navigate to={'/List'} replace={true} />}
 			<p>Welcome to your smart shopping list!</p>
 			<button onClick={handleClick}>Create New List</button>
 		</div>
