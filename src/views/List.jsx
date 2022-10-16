cf689d82c57713ea91dca11bcc7a779081a66ea1
Tina Flower <tmflower@gmail.com>
2022-10-16 12:30:18 -1000

    added filter feature

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,13 +1,51 @@
 import { ListItem } from '../components';
+import { useState } from 'react';
 
 export function List({ data }) {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const onSearch = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
+	const onSearchReset = () => {
+		setSearchTerm('');
+	};
+
+	const filteredData = data.filter((item) =>
+		item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+	);
+
 	return (
 		<>
-			<ul>
-				{data.map((item, i) => (
-					<ListItem key={item.name + i} {...item} />
-				))}
-			</ul>
+			<form>
+				<label htmlFor="searchTerm">Filter list items</label>
+				<input
+					type="text"
+					value={searchTerm}
+					id="searchTerm"
+					placeholder="Start typing here..."
+					onChange={onSearch}
+				></input>
+				{searchTerm === '' ? null : (
+					<button onClick={onSearchReset} type="reset">
+						Reset
+					</button>
+				)}
+				{/* @TODO Pressing 'enter' should not activate reset */}
+			</form>
+			{
+				filteredData.length > 0 ? (
+					<ul>
+						{filteredData.map((item, i) => (
+							<ListItem key={item.name + i} {...item} />
+						))}
+					</ul>
+				) : (
+					<p>No items match your search.</p>
+				)
+				// @TODO Prevent this from displaying at initial load of List view
+			}
 		</>
 	);
 }
