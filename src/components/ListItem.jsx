7846a70a5882535a540b588915a985b07c13ffa8
a-cole55 <arniqua_c@yahoo.com>
2022-10-25 09:40:49 -0500

    added dateLastPurchase to handlePurchase function

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,13 +2,15 @@ import './ListItem.css';
 import { updateItem } from '../api/firebase';
 
 const currentTime = new Date();
+const numOfSecondsinMin = 60;
 
 export function ListItem({ name, item, listToken }) {
 	const { isChecked, id, totalPurchases, dateLastPurchased } = item;
 
 	if (isChecked) {
 		const timeDiffMinutes =
-			(currentTime.getTime() / 1000 - dateLastPurchased.seconds) / 60;
+			(currentTime.getTime() / 1000 - dateLastPurchased.seconds) /
+			numOfSecondsinMin;
 		const minutesBeforeReset = 1440; // 24 hours x 60 minutes per hour
 		if (timeDiffMinutes >= minutesBeforeReset) {
 			const itemData = {
@@ -28,6 +30,7 @@ export function ListItem({ name, item, listToken }) {
 			const updatedPurchaseCount = totalPurchases + 1;
 			const itemData = {
 				isChecked: true,
+				dateLastPurchased: currentTime,
 				totalPurchases: updatedPurchaseCount,
 			};
 			updateItem(listToken, id, itemData);
