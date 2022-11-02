25ca20d26fd26783bbebf2cdb8f8b5074f7a6b99
Weston Norwood <weston.norwood@gmail.com>
2022-11-01 23:40:31 -0700

    Update getDaysBetweenDates function

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -10,22 +10,24 @@ const ONE_DAY_IN_MILLISECONDS = 86400000;
 export function getFutureDate(offset) {
 	return new Date(Date.now() + offset * ONE_DAY_IN_MILLISECONDS);
 }
+
 /**
- * Get a new JavaScript Date that is `offset` days in the future.
+ * Get the difference between two dates in days
  * @example
  * // Returns a Date 3 days in the future
  * getFutureDate(3)
- * @param {number} offset
+ * @param {Object} olderDate The date which occurs first chronologically
+ * @param {Object} newerDate The date which occurs second chronologically. Defaults to current date
+ * @returns {number} The difference between the dates in days
  */
 export function getDaysBetweenDates(olderDate, newerDate) {
-	if (!newerDate) {
-		newerDate = new Date();
-		return Math.abs(newerDate - olderDate.toDate()) / 60 / 60 / 24 / 1000;
-	} else {
+	if (newerDate) {
 		return (
-			Math.abs(newerDate.toDate() - olderDate.toDate()) / 60 / 60 / 24 / 1000
+			Math.abs(newerDate.toDate() - olderDate.toDate()) /
+			ONE_DAY_IN_MILLISECONDS
 		);
+	} else {
+		const currentDate = new Date();
+		return Math.abs(currentDate - olderDate.toDate()) / ONE_DAY_IN_MILLISECONDS;
 	}
-	// console.log(Math.abs(diff/60/60/24/1000));
-	// return new Date(Date.now() + offset * ONE_DAY_IN_MILLISECONDS);
 }
